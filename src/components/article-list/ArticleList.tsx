9084abd8a5142e78bfcb31a3e3ea3bbc7438f9d1
Alex <strelchenko.aleksandr@yandex.ru>
2026-04-30 10:30:05 +0300

    feat: add not-found message for empty Article List

--- src/components/article-list/ArticleList.tsx
+++ src/components/article-list/ArticleList.tsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import type { Article } from '../../types/article';
 import { ArticleCard } from '../article-card/ArticleCard';
 import { ArticleSkeleton } from '../skeleton/ArticleSkeleton';
+import styles from './ArticleList.module.css';
 
 interface Props {
   articles: Article[];
@@ -25,6 +26,10 @@ export class ArticleList extends Component<Props> {
 
     if (error) {
       return <p className="error">{error}</p>;
+    }
+
+    if (articles.length === 0) {
+      return <p className={styles.notFound}>Nothing found</p>;
     }
 
     return (
