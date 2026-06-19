242fecbf4c78329dbfdf35b8941cfb7c5dcbceb2
Alex <strelchenko.aleksandr@yandex.ru>
2026-06-20 00:29:38 +0300

    chore: migrate build tooling from vite to next.js

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  allowedDevOrigins: ['172.30.0.1'],
+  turbopack: {
+    root: __dirname,
+  },
+};
+
+export default withNextIntl(nextConfig);
